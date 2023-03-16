afccf0f368b179258592e390b0983ff5d9270a47
Dmytrii Shypilov <95936002+Dmytrii-Shypilov@users.noreply.github.com>
2023-03-16 21:07:53 +0200

    added classes templates

--- note_book/note_book.py
+++ note_book/note_book.py
@@ -1,2 +1,44 @@
+import pickle
+import re
+
+from collections import UserDict
 from prettytable import PrettyTable
 
+
+########## CLASSES ##################
+
+class NoteBook(UserDict):
+    def add_note(self):
+        pass
+
+    def get_note(self):
+        pass
+
+    def delete_note(self):
+        pass
+
+    def get_notes_by_tag(self):
+        pass
+
+
+class Note:
+    def __init__(self, tag, title, content):
+        self.title = title
+        self.tag = tag
+        self.content = content
+
+
+
+class Tag:
+    def __init__(self, value):
+        self.value = value
+
+class Title:
+    def __init__(self, value):
+        self.value = value
+
+class Content:
+    def __init__(self, value):
+        self.value = value
+
+##############  MODULE FUNCTIONS ################
