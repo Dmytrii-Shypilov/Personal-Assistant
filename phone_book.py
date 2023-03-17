e5b8250dfe64fe302275ac945c5f4573453b4ecc
Kostiantyn-Liapkalo <lynx87@ukr.net>
2023-03-17 14:29:33 +0200

    Create from collections import UserDict

---
+++ phone_book.py
@@ -0,0 +1,28 @@
+from collections import UserDict
+from datetime import datetime
+import pickle
+from pathlib import Path
+import re
+
+
+class Field:
+
+    def __init__(self, value):
+        self._value = value
+
+    def __str__(self):
+        return self._value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        self._value = value
+
+
+class Name(Field):
+
+    def __str__(self):
+        return self._value.title()
