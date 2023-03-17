4b112afcf31d50bef12db515f589af11f97ec4f6
Kostiantyn-Liapkalo <lynx87@ukr.net>
2023-03-17 15:31:08 +0200

    Create function phonebook

--- phone_book.py
+++ phone_book.py
@@ -104,5 +104,101 @@ class Record:
         if phone:
             self.phones.append(phone)
 
-  
+    
+    # Phone
+    def add_phone(self, phone):
+        phone = Phone(phone)
+        self.phones.append(phone)
+
+    def change_phone(self, old_phone, new_phone):
+        old_phone = Phone(old_phone)
+        new_phone = Phone(new_phone)
+        for phone in self.phones:
+            if phone.value == old_phone.value:
+                phone.value = new_phone.value
+
+    def delete_phone(self, old_phone):
+        old_phone = Phone(old_phone)
+        for phone in self.phones:
+            if phone.value == old_phone.value:
+                self.phones.remove(phone)
+
+    # Email
+    def add_email(self, email):
+        email = Email(email)
+        self.emails.append(email)
+
+    def change_email(self, old_email, new_email):
+        old_email = Email(old_email)
+        new_email = Email(new_email)
+        for email in self.emails:
+            if email.value == old_email.value:
+                email.value = new_email.value
+
+    def delete_email(self, old_email):
+        old_email = Email(old_email)
+        for email in self.emails:
+            if email.value == old_email.value:
+                self.emails.remove(email)
+
+    # Date of birth
+    def add_birthday(self, birthday):
+        # Adding date of birth
+        birthday = Birthday(birthday)
+        self.birthday = birthday
+
+    def delete_birthday(self):
+        #Delete date of birth
+        self.birthday = None
+        
+    # Days until user's birthday
+    def days_to_birthday(self):
+        
+        if self.birthday.value:
+            try:
+                birthday_value = datetime.strptime(
+                    self.birthday.value, '%d/%m/%Y').date()
+                current_date = datetime.now().date()
+                user_date = birthday_value.replace(year=current_date.year)
+                self.delta_days = user_date - current_date
+
+                if 0 < self.delta_days.days:
+                    return f"\n{self.name}'s birthday will be in {self.delta_days.days} days.\n"
+                else:
+                    user_date = birthday_value.replace(year=user_date.year + 1)
+                    self.delta_days = user_date - current_date
+                    if 0 < self.delta_days.days:
+                        return f"\n{self.name}'s birthday will be in {self.delta_days.days} days.\n"
+            except ValueError:
+                return f'\nPlease, input date in format dd/mm/yyyy\n '
+        else:
+            return f'\nDate of birth is not found. Add day of birth.\n '
+
+    # Working with Address
+    def add_address(self, address):
+        address = Address(address)
+        self.address = address
+
+    def delete_address(self):
+        self.address = None
+
+    def contacts(self):
+        phon = []
+        result_phones = ''
+        result_emails = ''
+        if len(self.phones) > 0:
+            for i in self.phones:
+                phon.append(str(i))
+                result_phones = ", ".join(phon)
+        em = []
+        if len(self.emails) > 0:
+            for i in self.emails:
+                em.append(str(i))
+                result_emails = ", ".join(em)
+        return f"\nname: {str(self.name.value)};\n" \
+               f"phone: {result_phones};\n" \
+               f"e-mail: {result_emails};\n" \
+               f"birthday: {self.birthday};\n" \
+               f"address: {self.address};\n"\
+            "..................."
    
